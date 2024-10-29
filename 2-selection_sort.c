fb71b93725abc0e486c0ff64ebd498db33f4a363
ckchesire <christianck.chesire@gmail.com>
2024-10-30 00:07:01 +0300

    Implemented selection sort algorithm.

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: integer array of numbers to sort
+ * @size: size of the array
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int min_value_idx = 0, tmp = 0;
+
+	if (size > 1)
+	{
+		while (i < size)
+		{
+			min_value_idx = i;
+			for (j = i + 1; j < size ; j++)
+			{
+				if (array[j] < array[min_value_idx])
+				{
+					min_value_idx = j;
+				}
+			}
+			if (min_value_idx != 1)
+			{
+				tmp = array[i];
+				array[i] = array[min_value_idx];
+				array[min_value_idx] = tmp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+}
